06140abcb090ea206e7b7fbb33f0ace0abaa277f
Yi-99 <ylim.8299@gmail.com>
2025-01-16 00:36:52 -0700

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,6 +95,24 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href={process.env.NEXT_PUBLIC_CLIENT_URL + '/signup'}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Sign In
+				</a>
+
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href={process.env.NEXT_PUBLIC_CLIENT_URL + '/login'}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Login
+				</a>
       </footer>
     </div>
   );
